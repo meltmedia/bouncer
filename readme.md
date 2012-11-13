ea754e26d6a9e5f91474166795df63f5bc329a33
Josh Kennedy <kennedy.josh@gmail.com>
2012-11-13 10:28:52 -0700

    tweaks, updates, better logging, g2g

--- readme.md
+++ readme.md
@@ -10,3 +10,57 @@ Features:
 Todo:
 * Support multiple aws accounts
 * check against groups
+
+## Configuration
+
+Configuration files are kept in the config folder. Refer to the examples included.
+
+### AWS Configuration
+
+The aws configuration contains the Access ID and Secret Key, as well as what endpoints are available and active.
+
+    {
+      "active": ["us-east-1", "us-west-1"],
+      "id": "ID_HERE",
+      "key": "KEY_HERE",
+      "regions": {
+        "us-west-1": "ec2.us-west-1.amazonaws.com",
+        "us-west-2": "ec2.us-west-2.amazonaws.com",
+        "us-east-1": "ec2.us-east-1.amazonaws.com",
+        "eu-west-1": "ec2.eu-west-1.amazonaws.com",
+        "sa-east-1": "ec2.sa-east-1.amazonaws.com",
+        "ap-southeast-1": "ec2.ap-southeast-1.amazonaws.com",
+        "ap-southeast-2": "ec2.ap-southeast-2.amazonaws.com",
+        "ap-northeast-1": "ec2.ap-northeast-1.amazonaws.com"
+      }
+    }
+
+### Server Configuration
+
+The server configuration sets the port to listen on, whether or not to use ssl, if ssl is enabled what paths to find the certificates at, what region the server is running in (differentiate when to use public or private ips), and what folder to server content from. If no content folder is specified it defaults to the current directory /public.
+
+For more ssl options see http://nodejs.org/api/tls.html#tls_tls_createserver_options_secureconnectionlistener
+
+Support for serving multiple directories is supported. Each entry under public will be configured to server content. 
+
+    {
+      "port": 4010,
+      "sslport": 4013,
+      "home": "us-east-1",
+      "ssl": true,
+      "ssl_options": {
+        "cert": "./ssl/star_meltdev_com.crt",
+        "key": "./ssl/star_meltdev_com.key",
+        "ca": ["./ssl/DigiCertCA.crt"]
+      },
+      "public": [
+        {
+          "context": "/example",
+          "path": "/Users/jkennedy/git/meltmedia/sysops/aws-web-server/public"
+        },
+        {
+          "context": "/",
+          "path": "/Users/jkennedy/git/meltmedia/sysops/aws-web-server/public"
+        }
+      ]
+    }
--- server.js
+++ server.js
@@ -12,64 +12,112 @@ var winstonStream = {
 };
 
 var express = require('express'),
+    https = require('https'),
+    http = require('http'),
     _ = require('underscore'),
     aws = require('aws-lib'),
     step = require('step'),
-    config = require('konphyg')(__dirname + '/config'),
+    fs = require('fs'),
+    konphyg = require('konphyg')(__dirname + '/config'),
     hosts = require('./lib/hosts');
 
 _.str = require('underscore.string');
 
+var config = konphyg.all();
+
 var app = express();
 
-var awsConfig = config('aws');
+if (config.server.ssl) {
+  try {
+    https.createServer(opencerts(config.server.ssl_options), app).listen(config.server.sslport);
+    test_ssl(config);
+    winston.info("SSL Server started on " + config.server.sslport);
+  }
+  catch (ex) {
+    winston.error("unable to bring up ssl server: " + ex);
+  }
+}
 
-app.listen(4010);
-winston.info("Server started on 4010")
+app.listen(config.server.port);
+winston.info("Server started on " + config.server.port)
+
+if (!isDefined(config.server.public)) {
+  config.server.public = [{ "context": "/", "path": __dirname + "/public" }]
+}
 
 // set the "home" region
-hosts.setHome("us-east-1");
+hosts.setHome(config.server.home);
 // Add us-east-1
-hosts.addEndpoint(aws.createEC2Client(awsConfig.id, awsConfig.key));
-// Add us-west-1
-hosts.addEndpoint(aws.createEC2Client(awsConfig.id, awsConfig.key, {host: awsConfig["us-west-1"]}))
 
+config.aws.active.forEach( function(endpoint) {
+  hosts.addEndpoint(
+    aws.createEC2Client(
+      config.aws.id, 
+      config.aws.key, 
+      { host: config.aws.regions[endpoint] }
+    )
+  );  
+});
+
+// Configure winston as the logger for express
 app.use(express.logger({stream:winstonStream}));
-app.use(express.static(__dirname + '/public'));
 
+// set the directories to server files from
+config.server.public.forEach(function(pub) {
+  try {
+    app.use(pub.context, express.static(pub.path));
+    winston.debug("[mount] " + pub.context + " serving " + pub.path);
+  }
+  catch (ex) {
+    winston.error("[mount] unable to mount " + pub.context + " with path " + pub.path);    
+  }
+});
+
+// Authentication mechanism for all requests
 app.all('*', function(req, res, next) {
   winston.debug("* check for ip/group for " + req.ip + " in " + hosts.ips.length);
+  // check if the requesters ip is allowed
   if (hosts.allow(req.ip)) {
     next();
   }
   else {
+    /* 
+      if the requester ip address was not found, 
+      reload IP addresses (in case it is a new instance)
+    */
     hosts.load(function(err) {
       if (err) {
+        // an error occurred while reloading the address list
         res.send(500, "unable to update allowed hosts");
         return;
       }
       
-      winston.info("** checking ip " + req.ip + " against " + hosts.ips.length);
+      winston.debug("** checking ip " + req.ip + " against " + hosts.ips.length);
       
+      // recheck against the allowed addresses
       if (hosts.allow(req.ip)) {
         next();
       }
       else {
+        // the address was still not allowed, send forbidden
         res.send(403, "you are not from ec2");
       }
     })
   }
 });
 
+// standard response for the root request
 app.get("/", function(req, res){
   res.send('200', "welcome to the machine");
 });
 
-app.get("/hosts", function(req, res) {
+// respond with a list of known addresses
+app.get("/system/hosts", function(req, res) {
   res.send('200', hosts.ips);
 });
 
-app.get("/reload", function(req, res) {
+// reload the list of known addresses
+app.get("/system/reload", function(req, res) {
   hosts.load(function(err) {
     if (err) {
       res.send(500, err);
@@ -80,8 +128,61 @@ app.get("/reload", function(req, res) {
   });
 });
 
+// initial population of known addresses
 hosts.load(function(err) {
   if (!err) {
     winston.debug("loaded hosts: " + hosts.ips.length);
   }
 });
+
+// Utils
+// simplify checking if a variable is defined
+function isDefined(obj) {
+  if (typeof(obj) != 'undefined' && obj != null) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+// Test the https endpoint, for now this is purely informative
+function test_ssl(options) {
+  winston.info("[https_test] testing https server");
+  https.get('https://localhost:' + options.server.sslport + "/", function(res) {
+    winston.info("[https_test] got a " + res.statusCode + " from the https server");
+  }).on('error', function(e) {
+    winston.error("[https_test] unable to connect to https server: " + e);
+  });
+}
+
+// Open the certificates used for https, safe gaurds are 
+// in place to allow calling this method more than once.
+function opencerts(options) {
+  if (isDefined(options.cert) && _.isString(options.cert)) {
+    options.cert = fs.readFileSync(options.cert);
+  }
+
+  if (isDefined(options.key) && _.isString(options.key)) {
+    options.key = fs.readFileSync(options.key);
+  }
+
+  if (isDefined(options.ca)) {
+    cas = [];
+    if (_.isArray(options.ca)) {
+      options.ca.forEach(function(ca) {
+        if (_.isString(ca)) {
+          cas.push(fs.readFileSync(ca));
+        }
+      })
+    } else if (_.isString(options.ca)) {
+      cas.push(fs.readFileSync(options.ca));
+    }
+
+    if (cas.length > 0) {
+      options.ca = cas;
+    }
+  }
+
+  return options;
+}
